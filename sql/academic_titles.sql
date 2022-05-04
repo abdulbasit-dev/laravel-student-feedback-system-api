84e1bf559d7b57318ec40aff9f9758f0d80068da
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-05-05 00:52:26 +0300

    sql file

--- sql/academic_titles.sql
+++ sql/academic_titles.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.7
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 02, 2022 at 02:16 AM
--- Server version: 5.6.51
--- PHP Version: 7.3.32
+-- Host: 127.0.0.1
+-- Generation Time: May 04, 2022 at 09:19 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `academic_qa`
+-- Database: `tables`
 --
 
 -- --------------------------------------------------------
